ec872d2665ee6a4e39eccec147fd84ed83b6cfe9
Matthew Stead <mattstead6@gmail.com>
2022-02-27 22:35:48 -0500

    commit tree 8b58e60da70bcf7c66c6bb9489bfdd791274e508

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+ 
+
+// function superbowlWin(recordsArray){
+//     const foundRecord = recordsArray.find(record => record.result === "W")
+//     return foundRecord
+// }
+
+
+// superbowlWin(record)
+
+
+
+
+function superbowlWin(myArray){
+    let win =  myArray.find((rec) => rec.result === "W")
+    return (
+        win
+        ? win.year
+        : undefined
+    );
+}
